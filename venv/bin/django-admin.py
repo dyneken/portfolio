8f8d423d3b4a7f7c3f456859678fcf03309ed98f
Matt Kennedy <matt@k3nnedy.com>
2018-09-20 23:37:40 -0700

    Set Up Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/matt_kennedy/Desktop/Classes/03_Django/Portfo_Site/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
