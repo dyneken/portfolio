b548dafd78bf977d2031b4ccd068ad31e5df1bce
Matt Kennedy <matt@k3nnedy.com>
2018-09-21 15:14:46 -0700

    Add Jobs to Home Page

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Blog(models.Model):
-        image = models.ImageField(upload_to='images/')
         title = models.CharField(max_length=50)
-        body = models.CharField(max_length=500)
-        date = models.DateField
+        body = models.TextField()
+        date = models.DateTimeField(default=timezone.now)
+        image = models.ImageField(upload_to='images/')
